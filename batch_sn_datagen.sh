b7f9ef0268db0fe6829eedb0acf4fdbff0eb7c63
Ravi Shastri <rsshast@umich.edu>
2025-10-14 12:45:10 -0400

    sbatch

--- batch_sn_datagen.sh
+++ batch_sn_datagen.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=All
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=36
-#SBATCH --mem=10G
+#SBATCH --mem=2G
 #SBATCH --time=00:10:00
 #SBATCH --account=bckiedro0
 #SBATCH --partition=standard
