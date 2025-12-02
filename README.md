fa24a5c92720d6d1f59aada2417b8d9390dc7e84
Avi Skoczylas <abraham.skoczylas@gmail.com>
2025-12-02 12:45:06 -0500

    more detailed readme, removed ydata transformaion, took losses on all groups at once in ml

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-Machine learning to predict group flux moments up to p3 from group and cross section data. Output data generation with 1d sn solver
+# Machine Learning Applied to $S_n$ Transport 
+
+Ravi Shastri, Avi Skoczylas, Brian Kiedrowski
+
+Regions, cross sections and boundary conditions are generated.
+A 1d $S_n$ solver is used to provide determinisic solutions for the problem with a fixed source.
+A machine learning model is trained on the inputs and outputs of the solver and generates flux predictions
+
+# Requirements:
+  * Python=3.11.0
+  * numpy=2.0.2
+  * pandas=2.2.3
+  * matplotlib
+  * scipy=1.15.1
+  * numba=0.62.1
+  * seaborn
+  * tensorflow=2.18.0
+  * scikit-learn=1.7.2
+
+# Usage:
+## sn_solver_fixed_source.py:
+  * $S_N$ class
+    * Generates problem geometry and cross sections if not provided up to specified order
+    * Defines fixed source using spatial and groupwise fourier coeficients
+  * Hydrogen ratio and boundary conditions are randomized for each solve
+  * Solves transport in 1D using discrete ordinates method
+    * Outer loop for group-to-group scattering, inner loop for within-group scattering
+    * Uses Numba to enhance performance
+  * Outputs xs, sources, BCs into csv to be used as xtrain data
+  * Outputs moments to be used as ytrain data
+  * Plots scalar and angular group flux profiles
+  * Other sn_solver files are variations of this solver
+
+## ml.py:
+  * Creates neural network using Tensorflow with custom loss function
+    * Loss function reconstructs groupwise spectra from fourier coefficients
+    * The loss is the sum of the L2 error for each group's reconstruction 
+  * Searches for optimal hyperparameters and saves them to np file
+  * Trains on input dataset containing xs, sources, and BCs
+  * Produces prediction of group flux moments
+  * Plots error metrics
+    * Residuals
+    * Flux reconstruction for best and worst indices
+    * Model learning curve
+    
+Completed Work:
+  * Implemented multiple types of $S_n$ solvers
+  * Generated training data
+  * Trained machine learning model to produce output with MSE error of $< 0.1$
+
+Current work: 
+  * Refine machine learning model, particularly the loss function, to reduce error
+  * Reduce amount of input data required for single region problems
+
+Future Work:
+  * Perform a machine learning sweep to apply results to multi region problems
+  * Extend results to higher orders of anisotropy
--- ml.py
+++ ml.py
@@ -1,67 +1,89 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import time
 import os
-from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.optimizers import Adam
 from scipy.special import roots_legendre
+from sklearn.preprocessing import MinMaxScaler
+from scipy.special import lpmv
+from numpy.polynomial.legendre import leggauss
 
-#TODO: fix data read in
-#make sure fourier expansion is being used correctly (correct coeffs/order passed in)
-
+a = 1 #thickness, not given in xdata
 num_groups = 8
+num_nodes = 100
+num_ordinates = 16
 epochs = 50
 num_hp_trials = 20
 source_order = 4
-bc_order = 3
-bc_data_in_y = 2*(bc_order+1)*num_groups
-# load data
-xdata = pd.read_csv("../data/xdata.csv")
-ydata = pd.read_csv("../data/ydata.csv")
-xtrain, xtest, ytrain, ytest = sklearn.model_selection.train_test_split(xdata, ydata, test_size=None, train_size=None, random_state=None, shuffle=True, stratify=None)
-#xtrain = pd.read_csv("../data/xtrain.csv")
-#ytrain = pd.read_csv("../data/ytrain.csv")
-xtrain = xtrain.to_numpy()
-ytrain = ytrain.to_numpy()
-#xtest = pd.read_csv("../data/xtest.csv").to_numpy()
-#ytest = pd.read_csv("../data/ytest.csv").to_numpy()
+bc_order = 4
+bc_data_in_y = 2*bc_order*num_groups
 
+# load data
+xtrain = pd.read_csv("data/xtrain1.csv").to_numpy()
+ytrain = pd.read_csv("data/ytrain1.csv").to_numpy()
+xtest = pd.read_csv("data/xtest1.csv").to_numpy()
+ytest = pd.read_csv("data/ytest1.csv").to_numpy()
+
+#remove first 264 or 776 columns for x data
+xtrain, xtest = xtrain[:,776:], xtest[:,776:]
+
+for sample in xtrain:
+    if sample[-1] == "reflecting":
+        sample[-1] = 0
+    elif sample[-1] =="vacuum":
+        sample[-1] = 1
+    elif sample[-1] =="inflow":
+        sample[-1] = 2
+for sample in xtest:
+    if sample[-1] == "reflecting":
+        sample[-1] = 0
+    elif sample[-1] =="vacuum":
+        sample[-1] = 1
+    elif sample[-1] =="inflow":
+        sample[-1] = 2
+
+#optionally truncate number of samples for speed.
+reduction_factor = 1
+train_samples = xtrain.shape[0]
+test_samples = xtest.shape[0]
+xtrain, ytrain = xtrain[:train_samples//reduction_factor,:], ytrain[:train_samples//reduction_factor,:]
+xtest, ytest =  xtest[:test_samples//reduction_factor,:], ytest[:test_samples//reduction_factor,:]
 # scale data
+
 xscaler = MinMaxScaler()
 xtrain = xscaler.fit_transform(xtrain)
 xtest = xscaler.transform(xtest)
-yscaler = MinMaxScaler()
+'''yscaler = MinMaxScaler()
 ytrain = yscaler.fit_transform(ytrain)
 ytest = yscaler.transform(ytest)
-
+'''
 # search space
 param_space = {
     'learning_rate': np.random.uniform(0.0001, 0.001, 10).tolist(),
-    'num_layers': np.random.randint(1, 6, 10).tolist(),
-    'num_nodes': [16, 32, 64, 128, 256],
+    'num_layers': np.random.randint(1, 10, 10).tolist(),
+    'num_layer_nodes': [16, 32, 64, 128, 256],
     'batch_size': [16, 32, 64, 128]
 }
 
-def flux_loss_wrapper(order, num_nodes, xtest):
+def flux_loss_wrapper(source_order, bc_order, num_nodes, xtest):
     #Returns a loss function that dynamically retrieves L_batch for each batch.
     def loss_fn(ytrue, ypred):
         batch_size = tf.shape(ytrue)[0]  # Get current batch size
         batch_indices = tf.range(batch_size)  # Generate indices for batch
         L_batch = tf.gather(xtest[:, 0], batch_indices)  # Extract L_batch dynamically
-
-        return _loss(ytrue, ypred, L_batch, order, num_nodes)
+        return _loss(ytrue, ypred, L_batch, source_order, bc_order, num_nodes)
 
     return loss_fn
 
 def groupwise_fourier_expansion(coeffs, order, num_nodes, num_groups, x):
-    #groupwise data is initially flattened into coeffs
-    x = tf.reshape(x, [1, num_nodes, 1])  # Shape: [num_nodes] -> [1, num_nodes, 1]
-    coeffs = tf.reshape(coeffs, [coeffs.shape[0], order*2+1, num_groups])
+    #groupwise data is initially flattened in coeffs
+    x = tf.reshape(x, [-1, num_nodes, 1])
+    x = tf.cast(x, tf.float32)
+    coeffs = tf.reshape(coeffs, [-1, order*2+1, num_groups])
     # First coefficient (broadcasted across x)
     expansion = tf.expand_dims(coeffs[:, 0, :], axis=1)  # Shape: [batch, 1, num_groups]
 
@@ -72,7 +94,23 @@ def groupwise_fourier_expansion(coeffs, order, num_nodes, num_groups, x):
 
     return expansion  # Shape: [batch, num_nodes, num_groups]
 
-def _loss(ytrue, ypred, L_batch, order, num_nodes):
+quadrature_points, weight = leggauss(num_ordinates)
+legendre = np.zeros((bc_order, num_ordinates))
+for l in range(bc_order):
+    for n in range(num_ordinates):
+        legendre[l, n] = lpmv(0, l, quadrature_points[n])
+legendre = tf.convert_to_tensor(legendre, dtype=tf.float32)
+
+def legendre_expansion(moments, bc_order, num_groups):
+    moments = tf.reshape(moments, [-1, bc_order, num_groups]) #shape [batch, bc_order, num_groups]
+    weights = tf.cast(tf.expand_dims(((2*tf.range(bc_order)+1)/2), -1), tf.float32)
+    weighted_moments = weights*moments  #shape [batch, bc_order, num_groups]
+    #direction does not matter since this is only for loss
+    angular_flux = tf.matmul(tf.transpose(weighted_moments, perm = [0,2,1]), legendre[:,:num_ordinates//2])  #shape [batch, num_groups, num_ordinates//2]
+    angular_flux = tf.transpose(angular_flux, perm = [0,2,1])  #shape [batch, num_ordinates//2, num_groups]
+    return angular_flux
+
+def _loss(ytrue, ypred, L_batch, s_order, b_order, num_nodes):
     """
     Computes L2 loss on the Gauss-Legendre integral of the reconstructed spectrum.
     
@@ -87,49 +125,58 @@ def _loss(ytrue, ypred, L_batch, order, num_nodes):
     xi, wi = roots_legendre(num_nodes)
     xi = tf.convert_to_tensor(xi, dtype=tf.float32) 
     wi = tf.convert_to_tensor(wi, dtype=tf.float32)
-    L_batch = tf.reshape(L_batch, [-1, 1])  
+    L_batch = tf.cast(tf.reshape(L_batch, [-1, 1]), tf.float32)  
     x_batch = (L_batch / 2) * (xi + 1) 
-     
-    # Reconstruct spectra from Fourier coefficients
-    phi     = groupwise_fourier_expansion(ytrue[bc_data_in_y:], order, num_nodes, num_groups, x_batch)  
-    phi_pred = groupwise_fourier_expansion(ypred[bc_data_in_y:], order, num_nodes, num_groups, x_batch)  
+    phi     = groupwise_fourier_expansion(ytrue[:,bc_data_in_y:], s_order, num_nodes, num_groups, x_batch)  
+    phi_pred = groupwise_fourier_expansion(ypred[:,bc_data_in_y:], s_order, num_nodes, num_groups, x_batch)  
 
     # Evaluate loss separately for each group
-    total_loss = 0
-    for i in range(num_groups):
-        phi_integral     = tf.matmul(phi[:,:,i],     tf.expand_dims(wi, axis=-1))  
-        phi_pred_integral = tf.matmul(phi_pred[:,:,i], tf.expand_dims(wi, axis=-1))  
+    phi_loss = 0
+    #for i in range(num_groups):
+    #    phi_integral     = tf.matmul(phi[:,:,i],     tf.expand_dims(wi, axis=-1))  
+    #    phi_pred_integral = tf.matmul(phi_pred[:,:,i], tf.expand_dims(wi, axis=-1))  
+    #    phi_loss += tf.reduce_mean(tf.square(phi_integral - phi_pred_integral)) 
+
         # Compute L2 norm of integral error
-        total_loss += tf.reduce_mean(tf.square(phi_integral - phi_pred_integral))
+
+    phi_loss += tf.reduce_mean(tf.square(phi - phi_pred)) 
+    #for some reason this works better than the commented groupwise method?
+
+    bc_loss = 0
+    # bc_l     = legendre_expansion(ytrue[:,:bc_data_in_y//2], b_order, num_groups)
+    # bc_l_pred = legendre_expansion(ypred[:,:bc_data_in_y//2], b_order, num_groups)
+    # bc_loss += tf.reduce_mean(tf.square(bc_l - bc_l_pred)) 
+
+    # bc_r     = legendre_expansion(ytrue[:,bc_data_in_y//2:bc_data_in_y], b_order, num_groups)
+    # bc_r_pred = legendre_expansion(ypred[:,bc_data_in_y//2:bc_data_in_y], b_order, num_groups)
+    # bc_loss += tf.reduce_mean(tf.square(bc_r - bc_r_pred)) 
+    
+    total_loss = phi_loss + bc_loss
 
     return total_loss
 
 # hp tuning
-BEST_PARAMS_FILE = "../models/tf_nn_hps.npy"
+BEST_PARAMS_FILE = "models/tf_nn_hps.npy"
 if os.path.exists(BEST_PARAMS_FILE):
     best_params = np.load(BEST_PARAMS_FILE, allow_pickle=True).item()
     print("Loaded best hyperparameters:", best_params)
 else:
     best_loss = float("inf")
     best_params = {}
-
     for _ in range(num_hp_trials):  
         lr = np.random.choice(param_space['learning_rate'])
         num_layers = np.random.choice(param_space['num_layers'])
-        num_nodes = np.random.choice(param_space['num_nodes'])
+        num_layer_nodes = np.random.choice(param_space['num_layer_nodes'])
         batch_size = np.random.choice(param_space['batch_size'])
-
-        # Build model----------------------------------------------------------------------------------------
         model = Sequential()
         model.add(Input(shape=(xtrain.shape[1],)))
         for _ in range(num_layers):
-            model.add(Dense(num_nodes, activation='relu'))
+            model.add(Dense(num_layer_nodes, activation='relu'))
         model.add(Dense(ytrain.shape[1], activation='linear'))
-#-------------------------------------------------
 
         #model.compile(optimizer=Adam(learning_rate=lr), loss='mse', metrics=['mae'])
         model.compile(optimizer=Adam(learning_rate=lr), 
-                        loss=flux_loss_wrapper(source_order, num_nodes, xtest), 
+                        loss=flux_loss_wrapper(source_order, bc_order, num_nodes, xtest), 
                         metrics=['mae'])
 
         history = model.fit(xtrain, 
@@ -144,8 +191,9 @@ else:
             best_loss = val_loss
             best_params = {'learning_rate': lr, 
                     'num_layers': num_layers, 
-                    'num_nodes': num_nodes, 
+                    'num_layer_nodes': num_layer_nodes, 
                     'batch_size': batch_size}
+        tf.keras.backend.clear_session()
     # Save 
     np.save(BEST_PARAMS_FILE, best_params)
     print("Saved best hyperparameters:", best_params)
@@ -153,10 +201,10 @@ else:
 model = Sequential()
 model.add(Input(shape=(xtrain.shape[1],)))
 for _ in range(best_params['num_layers']):
-    model.add(Dense(best_params['num_nodes'], activation='relu'))
+    model.add(Dense(best_params['num_layer_nodes'], activation='relu'))
 model.add(Dense(ytrain.shape[1], activation='linear'))
 model.compile(optimizer=Adam(learning_rate=best_params['learning_rate']), 
-                            loss=flux_loss_wrapper(source_order, num_nodes, xtest),
+                            loss=flux_loss_wrapper(source_order, bc_order, num_nodes, xtest),
                             metrics=['mae'])
 history = model.fit(
                 xtrain, 
@@ -176,42 +224,49 @@ plt.ylabel('Loss (MSE)')
 plt.yscale("log")
 plt.title('Learning Curve')
 plt.legend()
-plt.savefig("../charts/ml/tf_nn_learning_curve.png")
+plt.grid()
+plt.savefig("charts/ml/tf_nn_learning_curve.png")
 plt.close()
 
-#broadcasting issue?
-def group_sum_fourier_expansion(coeffs,order,num_nodes, num_groups):
+def fourier_expansion(coeffs,order,num_nodes, num_groups):
+    if coeffs.ndim == 1:
+        coeffs = np.expand_dims(coeffs,axis = 0)
     x = np.linspace(0, 1, num_nodes)
-    coeffs = np.reshape(coeffs, [order*2+1, num_groups])
+    coeffs = np.reshape(coeffs, [-1, order*2+1, num_groups]) 
     # Compute source by Fourier expansion
-    return coeffs[0, :] + sum(
-        coeffs[2 * k + 1, g] * np.cos(np.pi * (k + 1) * x)
-        + coeffs[2 * k + 2, g] * np.sin(np.pi * (k + 1) * x)
-        for k in range(order) for g in range(num_groups))
+    flux = []
+    for g in range(num_groups):
+        flux.append(np.zeros((coeffs.shape[0], num_nodes)))
+        flux[g] += np.expand_dims(coeffs[:,0,g], axis = -1)
+        for k in range(order):
+            flux[g] += np.outer(coeffs[:, 2 * k + 1, g], np.cos(np.pi * (k + 1) * x))
+            flux[g] += np.outer(coeffs[:, 2 * k + 2, g], np.sin(np.pi * (k + 1) * x))
+    return flux
+    #group_flux += np.expand_dims(coeffs[:, 0, :], axis = -1)
+    # for k in range(source_order):
+    #     group_flux += np.multiply.outer(coeffs[:, 2 * k + 1, :], np.cos(np.pi * (k + 1) * x))
+    #     group_flux += np.multiply.outer(coeffs[:, 2 * k + 2, :], np.sin(np.pi * (k + 1) * x))
+    # return np.sum(group_flux, axis = 1)
 
 #L2 norm on coeffs
 ypred = model.predict(xtest)
-ypred_transformed = yscaler.inverse_transform(ypred)
-ytest_transformed = yscaler.inverse_transform(ytest)
-
-df_pred = pd.DataFrame(ypred_transformed)
+'''ypred = yscaler.inverse_transform(ypred)
+ytest = yscaler.inverse_transform(ytest)
+'''
+df_pred = pd.DataFrame(ypred)
 df_pred.to_csv("ypred.csv",index = False)
-l2_errors = np.linalg.norm(ytest_transformed - ypred_transformed, axis=1)
+l2_errors = np.linalg.norm(ytest - ypred, axis=1)
 print(f"L2 Error on Output: {np.mean(l2_errors):.5f}")
 
 # dataset L2 error
-phi = np.zeros((ytest.shape[0],num_nodes))
-phi_hat = np.zeros_like(phi)
+phi     = sum(fourier_expansion(ytest[:, bc_data_in_y:],source_order,num_nodes,num_groups))
+phi_hat = sum(fourier_expansion(ypred[:, bc_data_in_y:],source_order,num_nodes,num_groups))
 
-for ind in range(ytest.shape[0]):
-    phi[ind,:]     = group_sum_fourier_expansion(ytest[bc_data_in_y:],source_order,num_nodes,num_groups)
-    phi_hat[ind,:] = group_sum_fourier_expansion(ypred[bc_data_in_y:],source_order,num_nodes,num_groups)
-
-L2_phi = np.linalg.norm(phi - phi_hat)
-print(f"L2 norm on the scalar flux = {np.round(L2_phi,5)}")
+L2_phi = np.mean(np.linalg.norm(phi - phi_hat, axis = 1))
+print(f"L2 error on scalar flux = {np.round(L2_phi,5)}")
 
 # residuals
-residuals = np.abs(ytest[bc_data_in_y:] - ypred[bc_data_in_y:])
+residuals = np.abs(ytest[:, bc_data_in_y:] - ypred[:, bc_data_in_y:])
 plt.figure()
 plt.plot(residuals[:, 0], label="A0")
 plt.plot(residuals[:, 1], label="A1")
@@ -220,7 +275,8 @@ plt.title("Testing Residuals for Fourier Coeffs")
 plt.xlabel("Sample Index")
 plt.ylabel("Error")
 plt.yscale("log")
-plt.savefig("../charts/ml/tf_residuals.png")
+plt.legend()
+plt.savefig("charts/ml/tf_residuals.png")
 plt.close()
 
 best_idx = np.argmin(np.abs(residuals), axis=0)[0]
@@ -230,27 +286,36 @@ x = np.linspace(0, 1, num_nodes)  # Define x in the same way as fourier_expansio
 
 def plot_flux(x, t, ytest, ypred, flux_order, num_nodes, idx, title):
     """Plot expansion and TT solution."""
-    plt.clf()
-    plt.plot(
-        t * x,
-        group_sum_fourier_expansion(ytest[bc_data_in_y:], flux_order, num_nodes, num_groups),
-        label="Y-Test",
-    )
-    plt.plot(
-        t * x,
-        group_sum_fourier_expansion(ypred[bc_data_in_y:], flux_order, num_nodes, num_groups),
-        label="Y-Pred",
-    )
-    plt.xlabel("$x$")
-    plt.ylabel("$\\phi(x)$")
-    plt.legend()
-    plt.title(title)
-    plt.savefig(f"../charts/ml/tf_ytest_ypred_{idx}.png")
-    plt.close()
+    true_fluxes = fourier_expansion(ytest, flux_order, num_nodes, num_groups)
+    pred_fluxes = fourier_expansion(ypred, flux_order, num_nodes, num_groups)
+    for g in range(num_groups):
+        plt.clf()
+        plt.plot(
+            t * x,
+            np.squeeze(true_fluxes[g]),
+            label="Y-Test",
+        )
+        plt.plot(
+            t * x,
+            np.squeeze(pred_fluxes[g]),
+            label="Y-Pred",
+        )
+        plt.xlabel("$x$")
+        plt.ylabel("$\\phi(x)$")
+        plt.legend()
+        plt.title(f"{title} Group {g+1}")
+        plt.grid()
+        plt.savefig(f"charts/ml/tf_ytest_ypred_{idx}_group{g+1}.png")
+        plt.close()
+
+# print(ytest[best_idx, bc_data_in_y:])  # ytest coeffs
+# print("\n\n")
+# print(ypred[best_idx, bc_data_in_y:])   # ypred_coeffs
+
 
 plot_flux(
     x,
-    xtest[best_idx, 0],   # thickness
+    a,   # thickness
     ytest[best_idx, bc_data_in_y:],  # ytest coeffs
     ypred[best_idx, bc_data_in_y:],   # ypred_coeffs
     source_order,
@@ -261,7 +326,7 @@ plot_flux(
 
 plot_flux(
     x,
-    xtest[worst_idx, 0],   # thickness
+    a,   # thickness
     ytest[worst_idx, bc_data_in_y:],  # ytest coeffs
     ypred[worst_idx, bc_data_in_y:],   # ypred_coeffs
     source_order,
